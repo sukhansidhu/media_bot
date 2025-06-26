dd4d5a93a0278f816b52ea70e4faaf7cadb1474d
sukhansidhu <jashanpreetsingh.chotian@gmail.com>
2025-06-26 21:04:31 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,16 @@
+# media_bot/config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    DOWNLOAD_PATH = "downloads"
+    UPLOAD_PATH = "uploads"
+    MAX_TITLE_LENGTH = 10
+    ALLOWED_USERS = []  # Add user IDs if needed
+    MAX_FILE_SIZE = 2000 * 1024 * 1024  # 2GB
+    THUMBNAIL_DIR = "thumbnails"
+    METADATA_DIR = "metadata"
+    FFMPEG_PATH = "ffmpeg"
