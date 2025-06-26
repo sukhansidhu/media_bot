2e5a255211a97a4234ed264df37ba1fac4d389d3
sukhansidhu <jashanpreetsingh.chotian@gmail.com>
2025-06-26 21:16:16 +0530

    Create start.py

---
+++ handlers.py/start.py
@@ -0,0 +1,15 @@
+# media_bot/handlers/start.py
+from telegram import Update
+from telegram.ext import ContextTypes, CommandHandler
+from utils.buttons import main_menu_keyboard
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    await update.message.reply_text(
+        f"Hello {user.first_name}! Welcome to Media Bot.\n"
+        "Please select an option from the menu below:",
+        reply_markup=main_menu_keyboard()
+    )
+
+def setup_start_handlers(application):
+    application.add_handler(CommandHandler("start", start))
